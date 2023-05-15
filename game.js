58575d7643e5fbfb6f14d3f25d0abd5b5b4520ac
Daniel <dannyjaygil@gmail.com>
2023-05-15 09:45:31 +0300

    Make user input case insensitive

--- game.js
+++ game.js
@@ -4,25 +4,24 @@ function getComputerChoice() {
     return randomChoice;
 }
 
-playerInput = prompt("Enter your choice!");
+playerInput = prompt("Enter your choice!").toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a draw! Try again!";
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    } else if (playerSelection === "rock" && computerSelection === "Paper") {
         return "You Lose! Paper beats Rock"
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "Scissors") {
         return "You Win! Rock beats Scissors"
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    } else if (playerSelection === "paper" && computerSelection === "Rock") {
         return "You Win! Paper beats Rock"
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
         return "You Lose! Scissors beats Paper"
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
         return "You Win! Scissors beats Paper"
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
         return "You Lose! Rock beats Scissors"
     }
-
 }
 
 const computerSelection = getComputerChoice();
