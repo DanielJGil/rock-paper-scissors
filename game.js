db396f1aaa9fc5b683b021e78fb9d7c90a89e668
Daniel <dannyjaygil@gmail.com>
2023-05-24 06:22:13 +0300

    Set main text to appear according to winner, add scoring system and improve CSS animation

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
-const playerScore = 0;
-const computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 playing = true;
 
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
+const playerPoints = document.querySelector(".playerscore");
+const computerPoints = document.querySelector(".computerscore");
 const playerChoiceRock = document.querySelector(".playerchoice--0");
 const playerChoicePaper = document.querySelector(".playerchoice--1");
 const playerChoiceScissors = document.querySelector(".playerchoice--2");
-const computerChoiceRock = document.querySelector(".computerchoice--0");
-const computerChoicePaper = document.querySelector(".computerchoice--1");
-const computerChoiceScissors = document.querySelector(".computerchoice--2");
+// const computerChoiceRock = document.querySelector(".computerchoice--0");
+// const computerChoicePaper = document.querySelector(".computerchoice--1");
+// const computerChoiceScissors = document.querySelector(".computerchoice--2");
 const computerChoice = document.querySelector(".rightimg");
+const mainText = document.querySelector(".maintext");
 
 rock.addEventListener("click", function () {
   playerChoiceRock.style.display = "block";
@@ -22,6 +25,18 @@ rock.addEventListener("click", function () {
 
   computerChoice.classList.remove("hidden");
   computerChoice.src = `img/${compChoice}.png`;
+
+  if (compChoice === 2) {
+    mainText.textContent = "You win! Rock beats Scissors!";
+    playerScore++;
+    playerPoints.textContent = playerScore;
+  } else if (compChoice === 1) {
+    mainText.textContent = "You lose! Paper beats Rock!";
+    computerScore++;
+    computerPoints.textContent = computerScore;
+  } else if (compChoice === 0) {
+    mainText.textContent = "It's a draw!";
+  }
 });
 
 paper.addEventListener("click", function () {
@@ -33,6 +48,18 @@ paper.addEventListener("click", function () {
 
   computerChoice.classList.remove("hidden");
   computerChoice.src = `img/${compChoice}.png`;
+
+  if (compChoice === 0) {
+    mainText.textContent = "You win! Paper beats Rock!";
+    playerScore++;
+    playerPoints.textContent = playerScore;
+  } else if (compChoice === 2) {
+    mainText.textContent = "You lose! Scissors beats Paper!";
+    computerScore++;
+    computerPoints.textContent = computerScore;
+  } else if (compChoice === 1) {
+    mainText.textContent = "It's a draw!";
+  }
 });
 
 scissors.addEventListener("click", function () {
@@ -44,4 +71,16 @@ scissors.addEventListener("click", function () {
 
   computerChoice.classList.remove("hidden");
   computerChoice.src = `img/${compChoice}.png`;
+
+  if (compChoice === 0) {
+    mainText.textContent = "You lose! Rock beats Scissors!";
+    computerScore++;
+    computerPoints.textContent = computerScore;
+  } else if (compChoice === 1) {
+    mainText.textContent = "You win! Scissors beats paper!";
+    playerScore++;
+    playerPoints.textContent = playerScore;
+  } else if (compChoice === 2) {
+    mainText.textContent = "It's a draw!";
+  }
 });
